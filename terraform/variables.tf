2f0373a13cd35769d43a5bf4a73cf66189bb44ab
akishorekumar-1728 <akishorekumar1728@gmail.com>
2026-06-26 11:51:44 +0530

    Add Terraform Azure infrastructure

---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "storage_account_name" {
+  type = string
+}
